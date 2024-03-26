592d8ce8104a74bb5b9cd2173d11a68b251905db
Piyush Bachani <Piyush>
2024-03-26 10:19:24 +0530

    Done

--- server/server.js
+++ server/server.js
@@ -3,8 +3,13 @@ const app = express()
 const port = process.env.PUBLIC_PORT || 3000
 
 app.use(express.json())
+
 app.get('/',(req,res)=>{
     res.json({message:"HI"})
+})
+
+app.post('/',async(req,res)=>{
+  res.json({message:"Post Request Accepted.."})
 })
 
 if (require.main === module) {
