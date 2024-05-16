581f63e129b08cfcc5049bb50296f03bfb57ba2a
Piyush Bachani <Piyush>
2024-05-16 14:58:50 +0530

    Done

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Link} from "react-router-dom"
 import "./Login.css"
+import axios from 'axios'
 
 export default function Login() {
+  
+  const viewPassword = () =>{
+    const x = document.getElementById("passwordBox")
+    if (x.type === 'password') {
+      x.type = 'text'
+    } else {
+      x.type = 'password'
+    }
+  }
+
   return (
     <div id='loginPage'>
       <div id='loginBox'>
         <h1 id='loginHead'>Login</h1>
         <form>
-          <label>Email or Username -</label><br />
-          <input type="text" id='usernameEmailBox' /><br /><br />
+          <label>Email -</label><br />
+          <input type="email" id='EmailBox' /><br /><br />
           <label>Password -</label><br />
-          <input type="password " id='passwordBox' /><br />
+          <input type="password" id='passwordBox' /><br />
           <div id='showPass'>
-            <input type="checkbox" id="showPasswordBox" /><h5 id='show'>Show Password</h5>
+            <input type="checkbox" id="showPasswordBox" onClick={viewPassword} /><h5 id='show'>Show Password</h5>
           </div>
           <br />
           <button id='loginBtn'>LOGIN</button>
